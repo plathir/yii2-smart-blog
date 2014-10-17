e30d44970d10d25502693d2427a15657296eb251
plathir <plathir@yahoo.com>
2014-10-17 16:23:01 +0300

    'initial'

---
+++ controllers/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace plathir\smartblog\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
+
