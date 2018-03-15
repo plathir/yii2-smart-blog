4bb55a4f8292d4d1ba9d08c8241cfcf2d80e6367
iq10255 <iq10255@iq10255.infoquest.local>
2018-03-15 16:00:48 +0200

    Blog backend carousel

---
+++ frontend/models/Carousel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace plathir\smartblog\frontend\models;
+
+class Carousel extends \plathir\smartblog\common\models\Carousel {
+
+    use \plathir\smartblog\frontend\traits\ModuleTrait;
+}
---
+++ frontend/models/CarouselItems.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace plathir\smartblog\backend\models;
+
+class CarouselItems extends \plathir\smartblog\common\models\CarouselItems {
+    
+    
+}
